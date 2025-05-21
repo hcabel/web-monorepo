3ecd902e32cd83971b2518030fee4c6b49540115
hcabel <coding@hugocabel.com>
2025-05-21 22:05:50 +1000

    Remove Cookies (pointless already tracked by DNS provider)
    Removing my attempt at doing simple telemetry stuff.
    I though it was kind of fun, but actually I cant be bother.
    And every page your going to is already tracked by CloudFlare without
    your consent --'

--- apps/frontend/portfolio/app/[locale]/layout.tsx
+++ apps/frontend/portfolio/app/[locale]/layout.tsx
@@ -2,9 +2,7 @@
 import "../global.scss";
 
 // Components
-import CookiePopup from "Components/CookiePopup/CookiePopup";
 import LocaleSelector from "Components/LocaleSelector/LocaleSelector";
-import PathnameWatcher from "Components/telemetry/PathnameWatcher";
 
 // Hooks
 import { LocaleProvider, Locales } from "./LocaleContext";
@@ -22,12 +20,10 @@ export default function LocaleLayout(props: ILocaleLayoutProps) {
 	return (
 		<html lang={props.params.locale}>
 			<body className="root" style={{ background: "grey" }}>
-				<PathnameWatcher />
 				{/* Allow to access the local from every where easily */}
 				<LocaleProvider value={props.params.locale}>
 					<LocaleSelector />
 					{props.children}
-					<CookiePopup />
 				</LocaleProvider>
 			</body>
 		</html>
